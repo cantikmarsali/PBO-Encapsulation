ff983bd83e509910e4e7f5c27b6e70809de287d7
user <user@LAPTOP-JK7QBIK4>
2026-03-06 05:14:07 -0500

    Add BankService class implementation

--- src/main/java/com/mycompany/encapsulationbanksystem/BankService.java
+++ src/main/java/com/mycompany/encapsulationbanksystem/BankService.java
@@ -9,5 +9,25 @@ package com.mycompany.encapsulationbanksystem;
  * @author user
  */
 public class BankService {
-    
+    public void deposit(BankAccount acc, double amount){
+        if(amount > 0){
+            acc.setBalance(acc.getBalance() + amount);
+            System.out.println("Deposit berhasil");
+        } else {
+            System.out.println("Jumlah deposit tidak valid");
+        }
+    }
+
+    public void withdraw(BankAccount acc, double amount){
+        if(amount > 0 && acc.getBalance() >= amount){
+            acc.setBalance(acc.getBalance() - amount);
+            System.out.println("Withdraw berhasil");
+        } else {
+            System.out.println("Saldo tidak cukup");
+        }
+    }
+
+    public void checkBalance(BankAccount acc){
+        System.out.println("Saldo saat ini: " + acc.getBalance());
+    }
 }
