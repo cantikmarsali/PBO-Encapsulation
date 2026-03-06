de7636851a9ab3f08a6a0adc9ddc2c6af6de55fa
user <user@LAPTOP-JK7QBIK4>
2026-03-06 04:57:14 -0500

    Add Account class implementation

--- src/main/java/com/mycompany/encapsulationbanksystem/BankAccount.java
+++ src/main/java/com/mycompany/encapsulationbanksystem/BankAccount.java
@@ -9,5 +9,77 @@ package com.mycompany.encapsulationbanksystem;
  * @author user
  */
 public class BankAccount {
-    
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+    private String email;
+    private String phoneNumber;
+
+    // Constructor kosong
+    public BankAccount() {
+        balance = 0;
+    }
+
+    // Constructor dengan parameter
+    public BankAccount(String accountNumber, String ownerName, double balance, String email, String phoneNumber) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getter
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setter dengan validasi
+    public void setOwnerName(String ownerName) {
+        if(ownerName != null && !ownerName.isEmpty()){
+            this.ownerName = ownerName;
+        } else {
+            System.out.println("Nama tidak boleh kosong");
+        }
+    }
+
+    public void setBalance(double balance) {
+        if(balance >= 0){
+            this.balance = balance;
+        } else {
+            System.out.println("Saldo tidak boleh negatif");
+        }
+    }
+
+    public void setEmail(String email) {
+        if(email.contains("@")){
+            this.email = email;
+        } else {
+            System.out.println("Email tidak valid");
+        }
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if(phoneNumber.length() >= 10){
+            this.phoneNumber = phoneNumber;
+        } else {
+            System.out.println("Nomor HP tidak valid");
+        }
+    }
 }
--- src/main/java/com/mycompany/encapsulationbanksystem/EncapsulationBankSystem.java
+++ src/main/java/com/mycompany/encapsulationbanksystem/EncapsulationBankSystem.java
@@ -14,3 +14,4 @@ public class EncapsulationBankSystem {
         System.out.println("Hello World!");
     }
 }
+
