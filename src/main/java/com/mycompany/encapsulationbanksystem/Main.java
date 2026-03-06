fee58ffe61c918fa5e1467d65a88e233cc9393a4
user <user@LAPTOP-JK7QBIK4>
2026-03-06 05:36:24 -0500

    Add Main program

--- src/main/java/com/mycompany/encapsulationbanksystem/Main.java
+++ src/main/java/com/mycompany/encapsulationbanksystem/Main.java
@@ -9,5 +9,19 @@ package com.mycompany.encapsulationbanksystem;
  * @author user
  */
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        BankAccount acc1 = new BankAccount("12345","Komang",1000000,"komang@email.com","08123456789");
+
+        BankService service = new BankService();
+
+        service.checkBalance(acc1);
+
+        service.deposit(acc1,500000);
+
+        service.withdraw(acc1,200000);
+
+        service.checkBalance(acc1);
+    }
 }
